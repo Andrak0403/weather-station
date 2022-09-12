226f0d18e77a57fe80ff4159b3d65cc6cdee4070
Repaterion <99962752+Repaterion@users.noreply.github.com>
2022-09-12 13:28:43 +0200

    Update readUNO.py

--- readUNO.py
+++ readUNO.py
@@ -1,9 +1,9 @@
 #!/dev/usr/python3
 
-import serial
+import serial # Importerar bilioteket för seriellkommunikation.
 
 while 1:
-   ser = serial.Serial('/dev/ttyACM0', 9600)
-   readText = ser.readline()
-   print (readText)
+   ser = serial.Serial('/dev/ttyACM0', 9600) # Lagrar datan i variabeln var.
+   readText = ser.readline()   # Readline läser ut och lagrar sista raden och skriver denna till variabeln readText.
+   print (readText)            # skriver ut datat hos variabeln readtext.
 
