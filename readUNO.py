2884bc9b4201194fef6e732686f7832066ee3000
Repaterion <99962752+Repaterion@users.noreply.github.com>
2022-09-28 19:47:33 +0200

    Add files via upload
    Ändra utskrift så allt är på en rad.
    omvandlat dataströmmen från UNO i bit-form till ascii-form vilket gjort att "b" försvann.

--- readUNO.py
+++ readUNO.py
@@ -1,9 +1,18 @@
-#!/dev/usr/python3
+#!/usr/bin/python3
 
-import serial # Importerar bilioteket för seriellkommunikation.
+import time
+import serial
 
+windPrint = ""
 while 1:
-   ser = serial.Serial('/dev/ttyACM0', 9600) # Lagrar datan i variabeln var.
-   readText = ser.readline()   # Readline läser ut och lagrar sista raden och skriver denna till variabeln readText.
-   print (readText)            # skriver ut datat hos variabeln readtext.
-
+   ser = serial.Serial('/dev/ttyACM0', 9600)
+   windPrint = ser.read().decode('ascii')
+   tempfil = open('/sys/bus/w1/devices/28-0000095c963e/temperature')
+   temperatur = tempfil.read()
+   windPrint = windPrint.replace(r'\n','')
+   if len(temperatur) > 0:
+      temperaturprint = (float(temperatur) / 1000)
+      temperaturprint = str(temperaturprint)
+      print(f'\rTemperaturen är: {temperaturprint[:4]} C och Vindhastigheten är: {windPrint} m/s', end='')
+   time.sleep(0.15)
+   tempfil.close()
